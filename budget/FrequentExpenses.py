2a9d73bedffd12e152d89eceeab5522b658681eb
Dave Keeney <david.keeney@emergencyreporting.com>
2021-02-18 16:42:37 -0800

    Module 1 remove list comprehension

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -5,7 +5,10 @@ from budget import Expense
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
 
-spending_categories = [expense.category for expense in expenses.list]
+#spending_categories = [expense.category for expense in expenses.list]
+spending_categories = []
+for expense in expenses.list:
+    spending_categories.append(expense.category)
 spending_counter = collections.Counter(spending_categories)
 top5 = spending_counter.most_common(5)
 categories, count = zip(*top5)
