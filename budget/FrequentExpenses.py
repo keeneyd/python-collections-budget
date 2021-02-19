442ac745eceb526371e9f99ac5ac12cf3708ee5a
Dave Keeney <david.keeney@emergencyreporting.com>
2021-02-18 16:36:38 -0800

    Completed Module 1

---
+++ budget/FrequentExpenses.py
@@ -0,0 +1,16 @@
+import collections
+import matplotlib.pyplot as plt
+from budget import Expense
+
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+
+spending_categories = [expense.category for expense in expenses.list]
+spending_counter = collections.Counter(spending_categories)
+top5 = spending_counter.most_common(5)
+categories, count = zip(*top5)
+
+fig, ax = plt.subplots()
+ax.bar(categories,count)
+ax.set_title('# of Purchases by Category')
+plt.show()
